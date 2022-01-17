b3e6b2c61d38845511a29675ea4ad6ed10beab08
SCcagg5 <sccagg5@gmail.com>
2022-01-17 03:17:51 +0100

    Update sso.php

--- sso.php
+++ sso.php
@@ -23,50 +23,51 @@ function sso_settings_register() {
     register_setting( 'rocketbonds_sso_settings', 'sso_key' );
 }
 
-function sso_settings_page() {
+function sso_input($option_name){
     $settings = array() ;
     $settings_str = file_get_contents(plugin_dir_path( __FILE__ ) . "/settings.json");
     if ( $settings_str != false ) {
       $settings = json_decode($settings_str, true);
     }
+    $key_exist = False;
+    if (array_key_exists($option_name, $settings) && strlen($settings[$option_name]) > 0 ) {
+        $key = $settings[$option_name]);
+        $key_exist = True;
+    } else {
+        $key = get_option($option_name);
+    }
+    $key = esc_attr($key);
+    $ret = '<input type="text" name="' . $option_name . '" value="' . $key . '" ' . ( $key_exist ? 'disabled' : '') .'/>';
+    if ( $key_exist ) {
+       $ret .= '<input type="hidden" name="' . $option_name . '" value="' . $key . '" ' . ( $key_exist ? 'disabled' : '') .'/>';   
+    }
+    return $ret;
+}
+
+function sso_settings_page() {
+    
     echo '<div class="wrap">' .
          '<STYLE> .columns {float: left!important; width: 50%!important;}</STYLE>' .
          '<h1>' . __( 'SSO settings', 'rocketbonds-sso' ) . '</h1>' .
-         '<form method="post" action="options.php">' .
-         settings_fields( 'rocketbonds_sso_settings' ) .
-         do_settings_sections( 'rocketbonds_sso_settings' ) .
-         '<table class="form-table">'.
+         '<form method="post" action="options.php">';
+    settings_fields( 'rocketbonds_sso_settings' );
+    do_settings_sections( 'rocketbonds_sso_settings' );
+    echo '<table class="form-table">'.
          '<tr class="columns" valign="top">' .
          '<th scope="row">SSO register ID</th>' .
-         '<td><input type="text" name="sso_register" value="' .
-         (array_key_exists('sso_register', $settings) ? esc_attr( $settings['sso_register']) : esc_attr( get_option('sso_register') ) )
-         . '" ' .
-         (array_key_exists('sso_register', $settings) ? 'disabled' : '') 
-         .'/></td>' .
+         '<td>' . sso_input('sso_register') . '</td>'.
          '</tr>' .
          '<tr class="columns" valign="top">' .
          '<th scope="row">SSO register KEY</th>' .
-         '<td><input type="text" name="sso_key" value="' .
-         (array_key_exists('sso_key', $settings) ? esc_attr( $settings['sso_key']) : esc_attr( get_option('sso_key') ) )
-         . '" ' .
-         (array_key_exists('sso_key', $settings) ? 'disabled' : '') 
-         .'/></td>' .
+         '<td>' . sso_input('sso_key') . '</td>'.
          '</tr>' .
          '<tr class="columns" valign="top">' .
          '<th scope="row">SSO frontend url</th>' .
-         '<td><input type="text" name="sso_front" value="' .
-         (array_key_exists('sso_front', $settings) ? esc_attr( $settings['sso_front']) : esc_attr( get_option('sso_front') ) )
-         . '" ' .
-         (array_key_exists('sso_front', $settings) ? 'disabled' : '') 
-         .'/></td>' .
+         '<td>' . sso_input('sso_front') . '</td>'.
          '</tr>' .
          '<tr class="columns" valign="top">' .
          '<th scope="row">SSO backend url</th>' .
-         '<td><input type="text" name="sso_back" value="' .
-         (array_key_exists('sso_back', $settings) ? esc_attr( $settings['sso_back']) : esc_attr( get_option('sso_back') ) )
-         . '" ' .
-         (array_key_exists('sso_back', $settings) ? 'disabled' : '') 
-         .'/></td>' .
+         '<td>' . sso_input('sso_back') . '</td>'.
          '</tr>' .
          '</table>' .
          submit_button() .
